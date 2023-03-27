62d15c324cd12fedcb8d40f1ff0f80b08921279d
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-03-27 19:11:09 +0100

    added route paths

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import './Styling/App.css'
 import NavigationBar from './Navbar';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import Cakes from "./pages/Cakes";
-// import Treats from "./pages/Treats";
-// import Gallery from "./pages/Gallery";
-// import Contact from "./pages/Contact";
-// import About from "./pages/About";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import Cakes from "./pages/Cakes";
+import Treats from "./pages/Treats";
+import Gallery from "./pages/Gallery";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -17,12 +17,11 @@ function App() {
           {/* <ScrollToTop /> */}
         <Routes>
           <Route path="/" element={<Home />} />
-          
-          {/* <Route path="/cakes" element={<Cakes />} /> */}
-          {/* <Route path="/treats" element={<Treats />} /> */}
-          {/* <Route path="/gallery" element={<Gallery />} /> */}
-          {/* <Route path="/about" element={<About />} /> */}
-          {/* <Route path="/contact" element={<Contact />} /> */}
+          <Route path="/cakes" element={<Cakes />} />
+          <Route path="/treats" element={<Treats />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </>

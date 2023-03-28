ef219050ddecb53a5eed31d85104d88c71314128
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-03-28 17:42:17 +0100

    Banner updated

--- src/components/BannerUI.js
+++ src/components/BannerUI.js
@@ -3,19 +3,32 @@ import "../Styling/Banner.css";
 
 const Banner = (props) => {
   return (
-    <div class="banner-container">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-5">
-            <h2 className="banner-title">{props.title}</h2>
-            <p className="p-container">{props.paragraph}</p>
-          </div>
-          <div class="col-sm-7">
-            <img src={props.imgsrc} alt="" class="banner-img" />
-          </div>
+    <div class=".container-fluid">
+      <div class="row" style={{backgroundColor: "#f5e9d7"}}>
+        <div class="col ">
+          <h4 className="text-center">{props.title}</h4>
+          <p className="text-left">{props.paragraph}</p>
+        </div>
+        <div class="col">
+        <img src={props.imgsrc} alt="" class="float-end" />
         </div>
       </div>
     </div>
+    // <div class="jumbotron">
+    //   <div class=".container-fluid">
+    //     <div class="row">
+    //       <div class="col-sm-5">
+    //         <h2 className="banner-title">{props.title}</h2>
+    //         <p className="p-container">{props.paragraph}</p>
+    //       </div>
+    //       <div class="col-sm-7">
+    //         <div className="over-flow">
+    //         <img src={props.imgsrc} alt="" class="banner-img" />
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 

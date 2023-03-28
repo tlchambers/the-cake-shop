8b12fd7dc528395119e4057c1d5066cfd8c01705
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-03-28 12:09:50 +0100

    Navbar updated

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,59 +1,68 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-import "./Styling/Navbar.css"
-import Logo from "./sabrinaLogo.png"
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "./Styling/Navbar.css";
+import Logo from "./SabrinaLogo.PNG";
+import { Button } from "react-bootstrap";
 
 function NavigationBar() {
   return (
     <>
-      <div className=".navbar">
-        {/* <Navbar bg="light" variant="light">
-          <Container>
-            <a href="/">
-              <img src={Logo} alt="logo" height="200" />
-            </a>
-            <Nav className="me-auto">
-             <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/cakes">Cakes</Nav.Link>
-              <Nav.Link href="/treats">Treats</Nav.Link>
-              <Nav.Link href="/about">About</Nav.Link>
-              <Nav.Link href="/contact">Contact</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar> */}
-        <nav class="navbar navbar-expand-lg navbar-light bg-white">
-    <div class="container-fluid">
-      <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
-        data-mdb-target="#navbarExample01" aria-controls="navbarExample01" aria-expanded="false"
-        aria-label="Toggle navigation">
-        <i class="fas fa-bars"></i>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarExample01">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item active">
-            <a class="nav-link" aria-current="page" href="/">HOME</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/cakes">CAKE</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/treats">TREATS</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/price">PRICE GUIDE</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/about">ABOUT</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/contact">CONTACT</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-      </div>
+      <nav class="navbar navbar-expand-md bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">
+            <img src={Logo} alt="" />
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/">
+                  HOME
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/cakes">
+                  CAKES
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/treats">
+                  TREATS
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/price-guide">
+                  PRICE GUIDE
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/about">
+                  ABOUT
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/contact">
+                  CONTACT
+                </a>
+              </li>
+            </ul>
+            {/* <form class="d-flex mb-2 me-4" role="search">
+            <input type="text" class="search-hover" name="" />
+            </form> */}
+          </div>
+        </div>
+      </nav>
     </>
   );
 }

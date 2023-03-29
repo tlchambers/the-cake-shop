b6319e20d25de48a451b320487cfd3728a9460a9
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-03-29 12:57:52 +0100

    Hero section landing page and text

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,7 +1,27 @@
 import React from "react";
+import "../Styling/HeroSection.css";
+import Footer from "./Footer";
 
 function HeroSection() {
-  return <div className="hero"></div>;
+  return (
+    <>
+      <div className="hero-container">
+        <img
+          src="https://images.unsplash.com/photo-1595272568891-123402d0fb3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MjB8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+          alt=""
+          className="hero-image"
+        />
+        <div className="text-container">
+          <small>Welcome To</small>
+          <h1>Boujie Delights</h1>
+          <a class="hero-btn" href="/cakes" role="button">
+            EXPLORE CAKES
+          </a>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default HeroSection;

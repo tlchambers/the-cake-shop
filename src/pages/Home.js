3473b193b2e4279390c6ceba700633255d956dcb
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-03-30 23:58:58 +0100

    Comment home section for now

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,59 +1,11 @@
 import React, { useState } from "react";
-import Carousel from "react-bootstrap/Carousel";
 import '../Styling/Home.css';
 import HeroSection from '../components/HeroSection'
 // import Footer from "../Footer";
 
 function Home() {
-  const [index, setIndex] = useState(0);
-
-  const handleSelect = (selectedIndex, e) => {
-    setIndex(selectedIndex);
-  };
-
   return (
     <>
-      {/* <div class="rotating-text-wrapper">
-      </div>
-      <div className=".carousel">
-        <Carousel activeIndex={index} onSelect={handleSelect}>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://images.unsplash.com/photo-1578985545062-69928b1d9587?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60"
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h5>First slide label</h5>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://images.squarespace-cdn.com/content/v1/5418a5bae4b0877352524377/1486353082473-QEGG1Z3EVP8TQJ079TD8/image-asset.jpeg?format=1500we"
-              alt="Second slide"
-            />
-            <Carousel.Caption>
-              <h5>Second slide label</h5>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://images.unsplash.com/photo-1588195538326-c5b1e9f80a1b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1650&q=80"
-              alt="Third slide"
-            />
-            <Carousel.Caption>
-              <h5>Third slide label</h5>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-      </div> */}
       <HeroSection />
     </>
   );

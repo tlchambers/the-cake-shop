fc7c42296086eac05bea966870863130151df23b
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-03-30 23:49:43 +0100

    Updated contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -19,70 +19,70 @@ function Contact() {
     <>
       <div className="form-container">
         <Form onSubmit={handleSubmit}>
-          <div class="row g-3">
-            <div class="col">
-              <label for="exampleFormControlInput1" class="form-label">
+          <div className="row g-3">
+            <div className="col">
+              <label for="exampleFormControlInput1" className="form-label">
                 First Name
               </label>
               <input
                 type="text"
-                class="form-control"
+                className="form-control"
                 aria-label="First name"
                 value={firstName}
                 onChange={(e) => setFirstName(e.target.value)}
               />
             </div>
-            <div class="col">
-              <label for="exampleFormControlInput1" class="form-label">
+            <div className="col">
+              <label for="exampleFormControlInput1" className="form-label">
                 Last Name
               </label>
               <input
                 type="text"
-                class="form-control"
+                className="form-control"
                 aria-label="Last name"
                 value={lastName}
                 onChange={(e) => setLastName(e.target.value)}
               />
             </div>
           </div>
-          <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">
+          <div className="mb-3">
+            <label for="exampleFormControlInput1" className="form-label">
               Email address
             </label>
             <input
               type="email"
-              class="form-control"
+              className="form-control"
               id="exampleFormControlInput1"
               placeholder="name@example.com"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
           </div>
-            <label for="exampleFormControlInput1" class="form-label">
-              Subject
-            </label>
-            <input
-              type="subject"
-              class="form-control"
-              id="exampleFormControlInput1"
-              value={subject}
+          <label for="exampleFormControlInput1" className="form-label">
+            Subject
+          </label>
+          <input
+            type="subject"
+            className="form-control"
+            id="exampleFormControlInput1"
+            value={subject}
             onChange={(e) => setSubject(e.target.value)}
-            />
-          <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">
+          />
+          <div className="mb-3">
+            <label for="exampleFormControlTextarea1" className="form-label">
               Message
             </label>
             <textarea
-              class="form-control"
+              className="form-control"
               id="exampleFormControlTextarea1"
               rows="3"
               value={message}
               onChange={(e) => setMessage(e.target.value)}
             ></textarea>
           </div>
-          <div class="col-12">
-            <button type="submit" class="btn btn-primary">
-              {/* <button type="submit" class="btn btn-primary" value={submit}
+          <div className="col-12">
+            <button type="submit" className="btn btn-primary">
+              {/* <button type="submit" className="btn btn-primary" value={submit}
             onChange={(e) => setSubmit(e.target.value)}> */}
               Submit
             </button>

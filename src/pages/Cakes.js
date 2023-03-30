a1572e90913842d0f4a31320b7d218c1961c9991
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-03-30 23:49:28 +0100

    Added text for cake banners

--- src/pages/Cakes.js
+++ src/pages/Cakes.js
@@ -11,7 +11,8 @@ function Cakes() {
        <Banner
         title="Bespoke Handcrafted Cakes"
         imgsrc="https://images.unsplash.com/photo-1486427944299-d1955d23e34d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mjh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-        paragraph="Boujie Delights specialises in creating bespoke cakes that are tailored to customers' specific needs. From weddings to birthdays, and everything in between. Offering a wide range of flavours, fillings, and decorations ensures that your cake perfectly matches your vision. Freshly baked buttercream cakes, cupcakes, and treats are not only delicious, but also visually pleasing. Located in Brockley, South East London. "
+        paragraph="Boujie Delights specialises in creating bespoke cakes that are tailored to customers' specific needs. Offering a wide range of flavours, fillings, and decorations ensures that your cake perfectly matches your vision. Freshly baked buttercream cakes, cupcakes, and treats are not only delicious, but also visually pleasing. Located in Brockley, South East London. "
+        paragraph2="Freshly baked buttercream cakes, cupcakes, and treats are not only delicious, but also visually pleasing. Located in Brockley, South East London. "
       />
       <Cards />
     </>

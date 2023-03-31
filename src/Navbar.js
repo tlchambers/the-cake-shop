c6afcccab215abec3c4c5371f961eb7dd0f98026
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-03-31 23:07:53 +0100

    Navbar updated, logo now resizes to screen

--- src/Navbar.js
+++ src/Navbar.js
@@ -8,7 +8,7 @@ import { Button } from "react-bootstrap";
 function NavigationBar() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-white fixed-top text-center">
+      <nav className="navbar navbar-expand-md bg-light fixed-top justify-content-center text-center">
         <div className=".container-fluid">
           <a className="navbar-brand" href="/">
             <img src={Logo} alt="" />
@@ -16,43 +16,45 @@ function NavigationBar() {
           <button
             className="navbar-toggler"
             type="button"
-            data-mdb-toggle="collapse"
-            data-mdb-target="#navbarExample01"
-            aria-controls="navbarExample01"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <i className="fas fa-bars"></i>
+            <span className="navbar-toggler-icon">
+              <i className="fas fa-bars"></i>
+            </span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarExample01">
-            <ul className="navbar-nav  mb-lg-0">
-              <li className="nav-item active">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="/">
                   HOME
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/cakes">
-                  CAKES
+                <a class="nav-link" href="/cakes">
+                  BESPOKE CAKES
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/treats">
-                  TREATS
+                <a class="nav-link" href="/treats">
+                  BESPOKE TREATS
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/price-guide">
+                <a class="nav-link" href="/price-guide">
                   PRICE GUIDE
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/about">
+                <a class="nav-link" href="/about">
                   ABOUT
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/contact">
+                <a class="nav-link" href="/contact">
                   CONTACT
                 </a>
               </li>

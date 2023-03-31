055fd45d355a4b66f94a7d5330e20d747b1ff856
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-03-31 22:15:43 +0100

    Added footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import '../Styling/Home.css';
 import HeroSection from '../components/HeroSection'
-// import Footer from "../Footer";
+import Footer from "../components/Footer";
 
 function Home() {
   return (
     <>
       <HeroSection />
+      <Footer />
     </>
   );
 }

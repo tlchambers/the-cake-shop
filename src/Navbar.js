de7ba7e2433fa71e7a253ec90c18fda57a53b429
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-03-31 23:08:44 +0100

    Removed imports that are not used

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,5 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
 import "./Styling/Navbar.css";
 import Logo from "./SabrinaLogo.PNG";
-import { Button } from "react-bootstrap";
 
 function NavigationBar() {
   return (

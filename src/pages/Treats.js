920ffa5cb8d60670d55eb6a78694d66a37b2de1e
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-03-31 23:15:47 +0100

    Treats banner text container updated

--- src/pages/Treats.js
+++ src/pages/Treats.js
@@ -12,7 +12,10 @@ function Treats() {
       <Banner
         title="Bespoke Handcrafted Treats"
         imgsrc="https://images.unsplash.com/photo-1586195830864-e4d9688815c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2FrZSUyMHBvcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
-        paragraph="Lush Cake mixture on a stick dipped in luxurious chocolate these beauties are hand made and will be wrapped and tied if needed."
+        paragraph="Lush Cake mixture on a stick dipped in luxurious chocolate these beauties are hand made and will be wrapped and tied if needed. Whether you're celebrating a birthday or any special
+        occasion, we work closely with you to craft a unique and
+        personalized creation that perfectly matches your vision and
+        needs."
       />
       <Cards />
       <Footer />

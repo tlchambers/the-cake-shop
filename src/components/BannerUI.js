ea1e51b0f8a2abb7c8cf1ce83eccd465e7825f6c
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-04-02 22:30:31 +0100

    Added button to banner

--- src/components/BannerUI.js
+++ src/components/BannerUI.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../Styling/Banner.css";
 
+
 const Banner = (props) => {
   return (
     <div className=".container-fluid">
@@ -9,6 +10,7 @@ const Banner = (props) => {
           <h5 className="">{props.title}</h5>
           <p className="text-left">{props.paragraph}</p>
           <p className="text-left">{props.paragraph2}</p>
+          <button className="text-left">{props.button}</button>
         </div>
         <div class="col">
         <img src={props.imgsrc} alt="" className="float-end" />

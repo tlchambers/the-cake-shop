cc299679ac819a8dd24ebf6e317cc5ecca0290a9
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-04-05 23:38:52 +0100

    updated pricing draft

--- src/pages/PriceGuide.js
+++ src/pages/PriceGuide.js
@@ -1,9 +1,4 @@
 import React from "react";
-import "../Styling/Cakes.css";
-// import "../../components/pages/Services";
-// // import CardItem from "../CardItem";
-import Banner from "../components/BannerUI";
-import Cards from "../components/Cards";
 import "../Styling/Pricing.css";
 import { GiStrawberry, GiRaspberry, GiChocolateBar } from "react-icons/gi";
 import { BiCookie } from "react-icons/bi";
@@ -14,11 +9,9 @@ function Treats() {
     <>
       <section className="hero">
         <div className="container">
-          <div class="row justify-content-start">
-            <div class="col-md-12 bk-img align-self-center">
+          <div class="row justify-content-center text-center">
+            <div class="col-md-6 justify-content-center">
               <div className="text-hero-bold">PRICE GUIDE</div>
-            </div>
-            <div class="col-6 align-self-center">
               <div className="text-hero-regular">
                 When it comes to design, the sky is the limit! Whether you have
                 a specific theme or color scheme in mind, or simply want
@@ -26,90 +19,115 @@ function Treats() {
                 create a cake that perfectly captures your vision.
               </div>
             </div>
-            <div class="col-6"></div>
+            <div class="col-6">
+              <img src="https://images.unsplash.com/photo-1534766555764-ce878a5e3a2b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDR8fHNsaWNlJTIwY2FrZXxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60" />
+            </div>
           </div>
 
-          <div class="row justify-content-start">
-            <div class="col-md-6 align-self-center">
-              <div className="text-label">Fillings</div>
-              <ul>
-                <li>
-                  <GiStrawberry /> Strawberry Jam
-                </li>
-                <li>
-                  <GiRaspberry /> Raspberry Jam
-                </li>
-                <li>
-                  <BiCookie /> Cookie & Creme
-                </li>
-                <li>
-                  <GiChocolateBar />
-                  Caramel
-                </li>
-                <li>
-                  <FaCookieBite /> Lotus Biscoff
-                </li>
-              </ul>
+          <div className="row justify-content-center no-gutters">
+            <div className="col-6 d-flex justify-content-center">
+              <img
+                src="https://images.unsplash.com/photo-1534766555764-ce878a5e3a2b?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDR8fHNsaWNlJTIwY2FrZXxlbnwwfDB8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+                className="rounded-circle img-fluid perfect-circle"
+                alt="cake flavors"
+              />
             </div>
-            <div class="col-md-6">
-              <img src="https://images.unsplash.com/photo-1619356070234-607a25a7171e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTN8fGNha2V8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" />
-            </div>
-            <div class="w-100"></div>
-            <div class="col-md-6">
-              <img src="https://images.unsplash.com/photo-1619356070234-607a25a7171e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTN8fGNha2V8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" />
-            </div>
-            <div class="col-md-6">
-              <div className="text-label">Flavour Combinations</div>
-              <ul>
-                <li>Vanilla Sponge & Vanilla Buttercream</li>
-                <li>Oreo Sponge & Oreo Buttercream</li>
-                <li>Caramel Sponge & Biscoff Buttercream</li>
-                <li>Red Velvet Sponge & Vanilla Buttercream</li>
-                <li>Chocolate Sponge & Chocolate Buttercream</li>
-              </ul>
-            </div>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col flavour-section-bg">
-            <div className="text-hero-sub-heading">
-              Boujie Delights uses the finest ingredients to create cakes that
-              are both beautiful and delicious. From classic flavors like
-              vanilla and chocolate, to more indulging options like lotus
-              biscoff and caramel.
-            </div>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col-md-6">
-            <img src="https://images.unsplash.com/photo-1619356070234-607a25a7171e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTN8fGNha2V8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" />
-          </div>
-          <div className="col-md-6">
-            <div>
-              <div className="text-hero-sub-heading">Fillings</div>
-              <div className="text-hero-regular">
-                The following fillings are available to order:
-                <ul>
-                  <li>RASPBERRY JAM</li>
-                  <li>STRAWBERRY JAM</li>
-                  <li>HERSEY'S COOKIES N CREME</li>
-                  <li>CARAMEL</li>
-                  <li>LOTUS BISCOFFF</li>
+            <div className="col-md-6 align-self-center d-flex flex-column">
+              <div className="text-label align-self-center mb-3">
+                Flavour Combinations
+              </div>
+              <div className="text-left">
+                <ul className="text-center">
+                  <li>Vanilla Sponge & Vanilla Buttercream</li>
+                  <li>Oreo Sponge & Oreo Buttercream</li>
+                  <li>Caramel Sponge & Biscoff Buttercream</li>
+                  <li>Red Velvet Sponge & Vanilla Buttercream</li>
+                  <li>Chocolate Sponge & Chocolate Buttercream</li>
                 </ul>
               </div>
             </div>
           </div>
-        </div>
-        <div className="row justify-content-center">
-          <div className="col-md-4">
-            <img src="https://images.unsplash.com/photo-1619356070234-607a25a7171e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTN8fGNha2V8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" />
+
+          <div class="row justify-content-center text-center no-gutters">
+            <div class="col-md-6 align-self-center d-flex flex-column">
+              <div className="text-label align-self-center mb-3">
+                Cake Fillings
+              </div>
+              <div className="text-hero-regular">
+                Boujie Delights uses the finest ingredients to create cakes that
+                are both beautiful and delicious. From classic flavors like
+                vanilla and chocolate, to more indulging options like lotus
+                biscoff and caramel.
+              </div>
+              <ul class="text-center">
+                <li class="d-flex align-items-center">
+                  <span class="icon">
+                    <GiStrawberry />
+                  </span>
+                  <span class="ml-2">Strawberry Jam</span>
+                </li>
+                <li class="d-flex align-items-center">
+                  <span class="icon">
+                    <GiRaspberry />
+                  </span>
+                  <span class="ml-2">Raspberry Jam</span>
+                </li>
+                <li class="d-flex align-items-center">
+                  <span class="icon">
+                    <BiCookie />
+                  </span>
+                  <span class="ml-2">Cookie & Creme</span>
+                </li>
+                <li class="d-flex align-items-center">
+                  <span class="icon">
+                    <GiChocolateBar />
+                  </span>
+                  <span class="ml-2">Caramel</span>
+                </li>
+                <li class="d-flex align-items-center">
+                  <span class="icon">
+                    <FaCookieBite />
+                  </span>
+                  <span class="ml-2">Lotus Biscoff</span>
+                </li>
+              </ul>
+            </div>
+            <div class="col-6 d-flex justify-content-center">
+              <img
+                src="https://images.unsplash.com/photo-1534766555764-ce878a5e3a2b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDR8fHNsaWNlJTIwY2FrZXxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60"
+                class="rounded-circle img-fluid perfect-circle"
+              />
+            </div>
           </div>
-          <div className="col-md-4">
-            <div>
-              <div className="text-label">What to say here...</div>
-              <div className="text-hero-regular">shhsh</div>
+
+          <div class="row justify-content-center text-center no-gutters">
+            <div class="col-6 d-flex justify-content-center">
+              <img
+                src="https://plus.unsplash.com/premium_photo-1675706599329-1bae6e5b36e7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDN8fGJ1dHRlcmNyZWFtJTIwY2FrZXxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60"
+                class="rounded-circle img-fluid perfect-circle"
+              />
+            </div>
+            <div class="col-md-6 align-self-center d-flex flex-column">
+              <div class="text-label align-self-start mb-3 text-center">
+                Flavour Combinations
+              </div>
+              <ul class="justify-content-center">
+                <li class="d-flex align-items-center">
+                  Vanilla Sponge & Vanilla Buttercream
+                </li>
+                <li class="d-flex align-items-center">
+                  Oreo Sponge & Oreo Buttercream
+                </li>
+                <li class="d-flex align-items-center">
+                  Caramel Sponge & Biscoff Buttercream
+                </li>
+                <li class="d-flex align-items-center">
+                  Red Velvet Sponge & Vanilla Buttercream
+                </li>
+                <li class="d-flex align-items-center">
+                  Chocolate Sponge & Chocolate Buttercream
+                </li>
+              </ul>
             </div>
           </div>
         </div>
